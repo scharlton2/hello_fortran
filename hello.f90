a2dfcf09bf0bbd39f847df24ee772c8694212f61
Charlton, Scott R. <charlton.scott.dev@gmail.com>
2026-05-26 13:28:54 -0600

    Implement OpenMP support in the Fortran program and update CMake configuration

--- hello.f90
+++ hello.f90
@@ -1,3 +1,35 @@
 program hello
-  print *, "Hello, World!"
+
+#ifdef _OPENMP
+    use omp_lib
+#endif
+
+    implicit none
+
+    integer :: thread_id
+    integer :: num_threads
+
+#ifdef _OPENMP
+
+    !$omp parallel private(thread_id)
+
+    thread_id = omp_get_thread_num()
+
+    if (thread_id == 0) then
+        num_threads = omp_get_num_threads()
+        print *, "OpenMP enabled"
+        print *, "Number of threads =", num_threads
+    end if
+
+    print *, "Hello from thread", thread_id
+
+    !$omp end parallel
+
+#else
+
+    print *, "OpenMP not enabled"
+    print *, "Hello from single-threaded program"
+
+#endif
+
 end program hello
